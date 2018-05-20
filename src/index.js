a63d136587b4c65e2a1806e6f435e814c419e880
sanjivkrish <sanjiv436.k@gmail.com>
2018-05-21 01:17:54 +0200

    Include "Material UI" components
    Signed-off-by: Sanjiv Krishnasamy <s.krishnasamy@campus.tu-berlin.de>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+createMuiTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

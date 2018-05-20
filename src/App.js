f2fc54851d6b15f34cc9dfca97bce5fec5f12cb9
sanjivkrish <sanjiv436.k@gmail.com>
2018-05-21 01:29:05 +0200

    App.js : Use child components and update UI to Material
    Signed-off-by: Sanjiv Krishnasamy <s.krishnasamy@campus.tu-berlin.de>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Web3 from 'web3';
 import * as factoryOperations from './factoryOperations';
 import * as tokenOperations from './tokenOperations';
+import AppBar from './components/AppBar';
+import TokenManager from './components/TokenManager';
+import ProductManager from './components/ProductManager';
 
 class App extends Component {
   state = {
-    account: '',
+    account: null,
     balance: 0,
-    tokenAddress : null
+    tokenAddress : null,
+    tokenDesc : null,
+    isPastEventsLoaded : false,
+    pastEvents: []
   };
 
   constructor (props) {
@@ -23,6 +28,7 @@ class App extends Component {
         console.log('No Web3 Detected...');
     }
 
+    // Retreive User account info
     window.web3.eth.getAccounts((err, accs) => {
       if (err != null) {
         console.log('Error fetching your accounts');
@@ -34,13 +40,19 @@ class App extends Component {
         return
       }
 
-      console.log("Active current account : " + accs[0]);
       window.web3.eth.defaultAccount = accs[0];
       this.setState({
         account: accs[0]
       })
+
+      // Update Balance of the user
       this.checkBalance();
+
+      // Create instance of a factory contract
       factoryOperations.createFactoryInstance('0x73218674b32Be8e359ee63a4F8898F6103e79d63');
+
+      // Update past tokens created by user
+      this.getPastEvents();
     });
   }
 
@@ -49,10 +61,13 @@ class App extends Component {
     window.web3.eth.getBalance(window.web3.eth.defaultAccount, (error, wei) => {
       if (!error) {
           var balance = window.web3.utils.fromWei(wei, 'ether');
-          console.log(balance);
+          balance = parseFloat(balance).toFixed(2);
+
           this.setState({
             balance : balance
           })
+      } else {
+        console.log('Issue with getBalance()');
       }
     });
   }
@@ -60,28 +75,49 @@ class App extends Component {
   // Get past events from an account
   getPastEvents = () => {
     factoryOperations.getPastEvents('TokenCreated', {
-      filter: {_from: "0xcC5f6541E7Fd08f7E0969A596f32A3100590a696"},
+      filter: {_from: this.state.account},
       fromBlock: 0,
       toBlock: 'latest'
-    }, function(error, events){
-      for (var i = 0; i < events.length; i++) {
-        console.log(events[i].returnValues);
+    }).then((events) => {
+      var pastEvents = [];
+
+      // Update past events info
+      for (var i = (events.length-1) ; i > 0; i--) {
+        pastEvents.push({
+          id : i,
+          contractAddress : events[i].returnValues[0],
+          desc : events[i].returnValues[1]
+        });
       }
-    });
+
+      this.setState({
+        isPastEventsLoaded : true,
+        pastEvents : pastEvents
+      });
+    })
   }
 
   // Call createToken method from TokenFactory
   createToken = (desc, srcAddresses, srcAmounts) => {
+    // Freeze UI until token is created
+    this.setState({
+      isPastEventsLoaded : false
+    });
+
     factoryOperations.createToken(desc, srcAddresses, srcAmounts).then((result) => {
       console.log('Token created successfully');
 
       const tokenAddress = result.events.TokenCreated.returnValues[0];
-      console.log('Token address : ' + tokenAddress);
 
-      //this.setCurrentTokenInstance(tokenAddress)
+      // Create instance of newly created token
       tokenOperations.createTokenInstance(tokenAddress);
+
+      // Update past events to include current token
+      this.getPastEvents();
+
       this.setState({
-        tokenAddress : tokenAddress
+        tokenAddress : tokenAddress,
+        tokenDesc : desc
       });
     }, function (err) {
       console.log('Error in sending a method' + err);
@@ -89,61 +125,56 @@ class App extends Component {
 
   }
 
-  increaseSupply = (srcBatches, srcAmounts, amount) => {
-    tokenOperations.increaseSupply(srcBatches, srcAmounts, amount).then((result) => {
-      console.log('Supply increased by ' + amount);
-      console.log(result.events.AddedBatch.returnValues)
-    }, function (err) {
-      console.log('Error in sending a method' + err);
-    });
+  // Allow child to update token info using setTokenAddress
+  setTokenAddress = (tokenAddress, desc) => {
+    if (tokenAddress) {
+      tokenOperations.createTokenInstance(tokenAddress);
+      this.setState({
+        tokenAddress : tokenAddress,
+        tokenDesc : desc
+      });
+
+      console.log('Token Instance Created');
+    } else {
+      this.setState({
+        tokenAddress : null,
+        tokenDesc : null
+      });
+    }
   }
 
   render() {
     return (
-      <div className="App">
-        <p className="App-intro">
-          Active account : <b>{this.state.account}</b>
-        </p>
-        <p className="App-intro">
-          Contract address : <b>0x73218674b32Be8e359ee63a4F8898F6103e79d63</b>
-        </p>
-        <p className="App-intro">
-          Balance : <b>{this.state.balance}</b>
-        </p>
-        <p className="App-intro">
-          <button onClick={this.checkBalance}>Check balance</button>
-          <button onClick={this.getPastEvents}>Get Past Events</button>
-        </p>
-        <p className="App-intro">
-          Input : <b>("wood", [ ], [ ])</b>
-        </p>
-        <p className="App-intro">
-          Token address : <b>{ this.state.tokenAddress }</b>
-        </p>
-        <p className="App-intro">
-          <button onClick={() => {
-            // Replace hardcoded value with user input
-            this.createToken('wood', [], [])
-            }}>
-            Create Token
-          </button>
-        </p>
-        {
-          this.state.tokenAddress ?
-          <p>
-            <span className="App-intro">
-              Input : <b>([ ], [ ], 1)  </b>
-            </span>
-            <span className="App-intro">
-              <button onClick={() => {
-                // Replace hardcoded value with user input
-                this.increaseSupply([], [], 1)
-                }}>
-                Increase supply
-              </button>
-            </span>
-          </p> : null
-          }
+      <div>
+        { this.state.account ?
+          <div>
+            <AppBar
+              tokenAddress={this.state.tokenAddress}
+              tokenDesc={this.state.tokenDesc}
+              accAddress={this.state.account}
+              accBalance={this.state.balance}
+              setTokenAddress={this.setTokenAddress}>
+            </AppBar>
+            {
+              this.state.tokenAddress == null ?
+                (
+                  this.state.isPastEventsLoaded ?
+                  <TokenManager
+                    createToken={this.createToken}
+                    pastEvents={this.state.pastEvents}
+                    setTokenAddress={this.setTokenAddress}>
+                  </TokenManager>
+                  :
+                  <div>Loading...</div>
+                )
+                :
+                <ProductManager>
+                </ProductManager>
+            }
+          </div>
+          :
+          <div>No account found</div>
+        }
       </div>
     );
   }

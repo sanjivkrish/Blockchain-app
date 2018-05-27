9ae06f26a77873e28dd6778ae40300e84ad199a5
sanjivkrish <sanjiv436.k@gmail.com>
2018-05-27 10:13:00 +0200

    Operations: Update ABIARRAY to comply with new Contract
    Signed-off-by: Sanjiv Krishnasamy <s.krishnasamy@campus.tu-berlin.de>

--- src/factoryOperations.js
+++ src/factoryOperations.js
@@ -3,50 +3,55 @@ let factoryInstance;
 // Create a factory contract instance from a fcatory address
 export const createFactoryInstance = (factoryAddress) => {
     const abiArray = [
-        {
-          "constant": false,
-          "inputs": [
-            {
-              "name": "_description",
-              "type": "string"
-            },
-            {
-              "name": "_source_addresses",
-              "type": "address[]"
-            },
-            {
-              "name": "_source_amounts",
-              "type": "uint256[]"
-            }
-          ],
-          "name": "createToken",
-          "outputs": [
-            {
-              "name": "",
-              "type": "address"
-            }
-          ],
-          "payable": false,
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": false,
-              "name": "contract_address",
-              "type": "address"
-            },
-            {
-              "indexed": false,
-              "name": "description",
-              "type": "string"
-            }
-          ],
-          "name": "TokenCreated",
-          "type": "event"
-        }
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_description",
+            "type": "string"
+          },
+          {
+            "name": "_source_addresses",
+            "type": "address[]"
+          },
+          {
+            "name": "_source_amounts",
+            "type": "uint256[]"
+          }
+        ],
+        "name": "createToken",
+        "outputs": [
+          {
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": false,
+            "name": "contract_address",
+            "type": "address"
+          },
+          {
+            "indexed": false,
+            "name": "description",
+            "type": "string"
+          },
+          {
+            "indexed": false,
+            "name": "sender",
+            "type": "address"
+          }
+        ],
+        "name": "TokenCreated",
+        "type": "event"
+      }
     ];
 
     factoryInstance = new window.web3.eth.Contract(abiArray, factoryAddress, {

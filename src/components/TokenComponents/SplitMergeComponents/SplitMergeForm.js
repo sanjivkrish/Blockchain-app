01f074516a114554e2bd07359b9988f23791d3db
sanjivkrish <sanjiv436.k@gmail.com>
2018-06-25 22:56:51 +0200

    SplitMergeForm: Get split count and amount from user using form
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

--- src/components/TokenComponents/SplitMergeComponents/SplitMergeForm.js
+++ src/components/TokenComponents/SplitMergeComponents/SplitMergeForm.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
 
 const styles = theme => ({
   formControl: {
@@ -32,9 +35,47 @@ class ComposedTextField extends React.Component {
 			super(props, context);
 
 			this.state = {
-				
+				splitedCount : 2,
+				splitAmount : [1, 1]
 			};
 		}
+
+		// Increase text box to get user input
+		splitedCountChanged = (event) => {
+			var splitAmount = [];
+
+			if (event.target.value < 2) {
+				return
+			}
+
+			for (var i = 0; i < event.target.value; i++) {
+				if (this.state.splitAmount.length > i) {
+					splitAmount[i] = this.state.splitAmount[i];
+				} else {
+					splitAmount[i] = 1;
+				}
+			}
+
+			this.setState({
+				splitedCount: event.target.value,
+				splitAmount: splitAmount
+			});
+		};
+
+		// Get user input of how it needs to be splitted
+		splitAmountChanged = props => event => {
+			var splitAmount = this.state.splitAmount;
+
+			if (event.target.value < 1) {
+				return
+			}
+
+			splitAmount[props] = parseInt(event.target.value, 0);
+
+			this.setState({
+				splitAmount: splitAmount
+			});
+		};
 
     render() {
       const { classes } = this.props;
@@ -42,16 +83,16 @@ class ComposedTextField extends React.Component {
       return (
         <div className={classes.container}>
 					{
-						this.props.selectedBatchList.length === 0 ?
+						this.props.selectedBatchList.length === 0 || (this.props.selectedBatchList.length === 1 && this.props.selectedBatchAmount[0] === 1)?
 						<div>
 							<div className={classes.containerItem}>
 								<Button variant="raised" color="secondary" className={classes.formControl} disabled>
-									Split
+									Merge
 								</Button>
 							</div>
 							<div className={classes.containerItem}>
 								<Button variant="raised" color="secondary" className={classes.formControl} disabled>
-									Merge
+									Split
 								</Button>
 							</div>
 						</div>
@@ -59,26 +100,50 @@ class ComposedTextField extends React.Component {
 						this.props.selectedBatchList.length === 1 ?
 						<div>
 							<div className={classes.containerItem}>
-								<Button variant="raised" color="secondary" className={classes.formControl}>
-									Split
-								</Button>
-							</div>
-							<div className={classes.containerItem}>
 								<Button variant="raised" color="secondary" className={classes.formControl} disabled>
 									Merge
 								</Button>
+							</div>
+							<div className={classes.containerItem}>
+								<Button variant="raised" color="secondary" className={classes.formControl} onClick={() => {this.props.splitBatch(this.state.splitAmount)}}>
+									Split
+								</Button>
+								<FormControl className={classes.formControl}>
+									<InputLabel htmlFor="srcAmount-simple">Split into</InputLabel>
+									<Input
+										id="srcAmount-simple"
+										type="number"
+										value={this.state.splitedCount}
+										onChange={this.splitedCountChanged}/>
+								</FormControl>
+								{
+									this.state.splitAmount.map((n, i) => {
+										return (
+											<div key={i}>
+												<FormControl className={classes.formControl}>
+													<InputLabel htmlFor="srcAmount-simple">Split into</InputLabel>
+													<Input
+														id="srcAmount-simple"
+														type="number"
+														value={this.state.splitAmount[i]}
+														onChange={this.splitAmountChanged(i)}/>
+												</FormControl>
+											</div>
+										);
+									})
+								}
 							</div>
 						</div>
 						:
 						<div>
 							<div className={classes.containerItem}>
-								<Button variant="raised" color="secondary" className={classes.formControl} disabled>
-									Split
+								<Button variant="raised" color="secondary" className={classes.formControl} onClick={this.props.mergeBatches}>
+									Merge
 								</Button>
 							</div>
 							<div className={classes.containerItem}>
-								<Button variant="raised" color="secondary" className={classes.formControl} onClick={this.props.mergeBatches}>
-									Merge
+								<Button variant="raised" color="secondary" className={classes.formControl} disabled>
+									Split
 								</Button>
 							</div>
 						</div>
